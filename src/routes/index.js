2a29989e70fd34f2e0bf78064e9eabbea1008b3c
Dang Quang Anh <anhcrafter@gmail.com>
2023-04-12 13:01:41 +0700

    dotenv

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import { testEvironmentVariable } from '../setting.js'
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) => res.status(200).json({
-  message: 'Welcome to Express API template',
-}));
+indexRouter.get('/', (req, res) => 
+  res.status(200).json({
+    message: 'Welcome to Express API template',
+    test: testEvironmentVariable,
+  })
+);
 
 export default indexRouter;
---
+++ src/setting.js
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const testEvironmentVariable = process.env.TEST_ENV_VARIABLE
