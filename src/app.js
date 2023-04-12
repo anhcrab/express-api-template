4753fdeec3036eee1bebaa4cc73b823bc4083215
Dang Quang Anh <anhcrafter@gmail.com>
2023-04-12 12:49:54 +0700

    eslint adn prettier

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
-import logger from 'morgan'
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import indexRouter from './routes/index.js'
+import logger from 'morgan';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import indexRouter from './routes/index';
 
-const app = express()
+const app = express();
 
-app.use(logger('dev'))
+app.use(logger('dev'));
 
-app.use(express.json())
-app.use(express.urlencoded({ 
-  extended: true,
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(cookieParser())
-app.use('/api', indexRouter)
+app.use(cookieParser());
+app.use('/api', indexRouter);
 
-export default app
+export default app;
--- src/bin/www.js
+++ src/bin/www.js
@@ -2,71 +2,70 @@
 /**
  * Module dependencies.
  */
-import debug from 'debug'
-import http from 'http'
-import app from '../app.js'
+import debug from 'debug';
+import http from 'http';
+import app from '../app';
 /**
  * Normalize a port into a number, string, or false.
  */
-const normalizePort = val => {
-  const port = parseInt(val, 10)
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
   if (Number.isNaN(port)) {
     // named pipe
-    return val
+    return val;
   }
   if (port >= 0) {
     // port number
-    return port
+    return port;
   }
-  return false
-}
+  return false;
+};
 
 /**
  * Get port from environment and store in Express.
  */
-const port = normalizePort(process.env.PORT || '3000')
-app.set('port', port)
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 /**
  * Create HTTP server.
  */
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 /**
  * Event listener for HTTP server "error" event.
  */
-const onError = error => {
+const onError = (error) => {
   if (error.syscall !== 'listen') {
-    throw error
+    throw error;
   }
-  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      alert(`${bind} requires elevated privileges`)
-      process.exit(1)
-      break
-    case 'EADDRINUSE':
-      alert(`${bind} is already in use`)
-      process.exit(1)
-      break
-    default:
-      throw error
+  case 'EACCES':
+    alert(`${bind} requires elevated privileges`);
+    process.exit(1);
+    break;
+  case 'EADDRINUSE':
+    alert(`${bind} is already in use`);
+    process.exit(1);
+    break;
+  default:
+    throw error;
   }
-}
+};
 
 /**
  * Event listener for HTTP server "listening" event.
  */
 const onListening = () => {
-  const addr = server.address()
-  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
-  debug(`Listening on ${bind}`)
-}
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+};
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port)
-server.on('error', onError)
-server.on('listening', onListening)
-
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
