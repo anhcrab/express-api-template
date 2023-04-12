962c4f87eb9d7fcb32cfc4187499f7ed19376a39
Dang Quang Anh <anhcrafter@gmail.com>
2023-04-12 12:28:22 +0700

    init project with es6

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import logger from 'morgan'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import indexRouter from './routes/index.js'
+
+const app = express()
+
+app.use(logger('dev'))
+
+app.use(express.json())
+app.use(express.urlencoded({ 
+  extended: true,
+}))
+
+app.use(cookieParser())
+app.use('/api', indexRouter)
+
+export default app
